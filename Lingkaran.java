5ae99e04350172b5cb5cbef040345c5a45cd0f2d
jutekberkelas <114629808+Jutekberkelas@users.noreply.github.com>
2024-03-28 11:00:34 +0700

    Add files via upload

---
+++ Lingkaran.java
@@ -0,0 +1,18 @@
+
+package bangundatar;
+
+public class Lingkaran extends BangunDatar{
+    int r;
+    
+    public Lingkaran(int r){
+        this.r = r;
+    }
+    
+    public float luas(){
+        return (float) (Math.PI * r * r);
+    }
+    
+    public float keliling(){
+        return (float) (2 * Math.PI * r);
+    }
+}
